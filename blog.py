b810358dd91ce6c15d36937568a1dd210f28d152
Ubuntu <ubuntu@ip-172-31-1-92.ap-northeast-2.compute.internal>
2016-07-05 06:16:19 +0000

    으 드뎌 숙제 끝냄

--- blog.py
+++ blog.py
@@ -23,18 +23,23 @@ def write():
             flash('글을 적어주세요')
 
         else:
-            comment = comments(request.form['name'], request.form['comment'])
             count = int(r.hget('Comments', 'Count'))
-            r.hset('Comments', '%d' % (count + 1), comment)
+            r.lpush('%d' % (count + 1), request.form['name'], request.form['comment'])
             r.hincrby('Comments', 'Count', 1)
-            flash('글이 저장되었습니다', 'success')
+            flash('글이 저장되었습니다')
             return redirect(url_for('post'))
     return render_template('write.html')
 
 @app.route('/post')
 def post():
     count = int(r.hget('Comments', 'Count'))
-    return render_template('post.html', count = count)
+    list = []
+    for num in range(1, count + 1):
+        name = r.lindex('%d' % num, '1').decode('utf-8')
+        comment = r.lindex('%d' % num, '0').decode('utf-8')
+        list.append(comments(name, comment))
+
+    return render_template('post.html', list = list, count = count)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True, port = 55555)
